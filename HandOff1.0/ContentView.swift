af358a92f7e4bfa09d9ccd56d4663a386245de0d
Joseph DeWeese <rangerboy901@icloud.com>
2022-05-13 23:03:43 -0500

    Initial Commit

---
+++ HandOff1.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HandOff1.0
+//
+//  Created by Joseph DeWeese on 5/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HandOff1.0/HandOff1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  HandOff1_0App.swift
+//  HandOff1.0
+//
+//  Created by Joseph DeWeese on 5/13/22.
+//
+
+import SwiftUI
+
+@main
+struct HandOff1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
